0523dd892cf72b4809b17b69f4b3a857ef982202
gabriel <innermond@yahoo.com>
2016-03-21 23:02:52 +0200

    adding drop

--- sql/printoo.sql
+++ sql/printoo.sql
@@ -1,12 +1,24 @@
--- entries
-create table `entries` (
-	type tinyint not null default 0,
-	label varchar(100) not null default '',
-	amount float not null default 1
-) engine = innodb;
+drop database if exists `printoo`;
+-- create utf8-mb4 database
+create database `printoo` character set = utf8mb4 collate = utf8mb4_unicode_ci;
+use printoo;
 
 -- types
+drop table if exists `types`;
 create table `types` (
-	label varchar(100) not null,
+	label varchar(100) not null primary key,
 	description varchar(255)
 ) engine = innodb;
+
+-- entries
+drop table if exists `entries`;
+create table `entries` (
+	type varchar(100) not null default 0,
+	label varchar(100) not null default '',
+	amount float not null default 1,
+	primary key (type, label),
+	foreign key (type) references types (label)
+	on delete restrict
+	on update cascade
+) engine = innodb;
+
